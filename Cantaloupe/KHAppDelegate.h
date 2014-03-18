b965e7522a867ca293ca1a22cce31e77d993f73c
Kevin Hwang <k3vinhwang@gmail.com>
2014-03-17 23:16:23 -0700

    Initial Commit

---
+++ Cantaloupe/KHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KHAppDelegate.h
+//  Cantaloupe
+//
+//  Created by Kevin Hwang on 3/17/14.
+//  Copyright (c) 2014 Kevin Hwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Cantaloupe/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Cantaloupe
+//
+//  Created by Kevin Hwang on 3/17/14.
+//  Copyright (c) 2014 Kevin Hwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "KHAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([KHAppDelegate class]));
+    }
+}
