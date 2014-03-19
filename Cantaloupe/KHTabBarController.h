5bd044cbc7e2873314b7c24d90d5b6f837e6738c
Kevin Hwang <k3vinhwang@gmail.com>
2014-03-18 23:54:45 -0700

    Add a UITabBarController to hold all the views

---
+++ Cantaloupe/KHTabBarController.h
@@ -0,0 +1,13 @@
+//
+//  KHTabBarController.h
+//  Cantaloupe
+//
+//  Created by Kevin Hwang on 3/18/14.
+//  Copyright (c) 2014 Kevin Hwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KHTabBarController : UITabBarController
+
+@end
---
+++ Cantaloupe/KHTabBarController.m
@@ -0,0 +1,26 @@
+//
+//  KHTabBarController.m
+//  Cantaloupe
+//
+//  Created by Kevin Hwang on 3/18/14.
+//  Copyright (c) 2014 Kevin Hwang. All rights reserved.
+//
+
+#import "KHTabBarController.h"
+
+@interface KHTabBarController ()
+
+@end
+
+@implementation KHTabBarController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+@end
