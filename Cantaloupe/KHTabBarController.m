c27461a4d521ec5929bad89d5684c2505ef4b100
Kevin Hwang <k3vinhwang@gmail.com>
2014-03-19 00:15:04 -0700

    Add (erroneous system contacts) bar item to the games view controller

--- Cantaloupe/KHTabBarController.m
+++ Cantaloupe/KHTabBarController.m
@@ -9,6 +9,10 @@
 #import "KHTabBarController.h"
 #import "KHGamesViewController.h"
 
+typedef NS_ENUM(NSInteger, KHTabViewControllerTag) {
+    KHGamesViewControllerTag
+};
+
 @interface KHTabBarController ()
 
 @property (nonatomic, strong) KHGamesViewController *gamesViewController;
@@ -22,9 +26,9 @@
     self = [super init];
     if (self) {
         self.gamesViewController = [[KHGamesViewController alloc] initWithStyle:UITableViewStylePlain];
+        self.gamesViewController.tabBarItem = [[UITabBarItem alloc] initWithTabBarSystemItem:UITabBarSystemItemContacts tag:KHGamesViewControllerTag];
         
         NSArray *viewControllers = @[self.gamesViewController];
-        
         [self setViewControllers:viewControllers];
     }
     return self;
