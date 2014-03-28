16a02b8691c3d01442dec87cbf1e12a360811d63
Kevin Hwang <k3vinhwang@gmail.com>
2014-03-27 18:58:08 -0700

    Center the login view

--- Cantaloupe/KHLoginView.m
+++ Cantaloupe/KHLoginView.m
@@ -24,6 +24,7 @@
         self.usernameField.placeholder = @"Username";
         self.usernameField.autocapitalizationType = UITextAutocapitalizationTypeNone;
         self.usernameField.returnKeyType = UIReturnKeyNext;
+        self.usernameField.textColor = [UIColor whiteColor];
         [self addSubview:self.usernameField];
         
         self.passwordField = [[UITextField alloc] initWithFrame:CGRectMake(0, 40.0f, frame.size.width, 20.0f)];
@@ -31,6 +32,7 @@
         self.passwordField.secureTextEntry = YES;
         self.passwordField.autocapitalizationType = UITextAutocapitalizationTypeNone;
         self.passwordField.returnKeyType = UIReturnKeyGo;
+        self.passwordField.textColor = [UIColor whiteColor];
         [self addSubview:self.passwordField];
         
         self.loginButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 60.0f, frame.size.width, 20.0f)];
@@ -42,8 +44,13 @@
 
 - (void)layoutSubviews {
     [self.usernameField sizeToFit];
-    [self.passwordField sizeToFit];
-    [self.loginButton sizeToFit];
+    CGFloat xPoint = floorf((self.frame.size.width - self.usernameField.frame.size.width) / 2);
+    
+    self.usernameField.frame = CGRectMake(xPoint, 20.0f, self.usernameField.frame.size.width, self.usernameField.frame.size.height);
+    
+    self.passwordField.frame = CGRectMake(xPoint, self.usernameField.frame.origin.y + 40.0f, self.usernameField.frame.size.width, self.usernameField.frame.size.height);
+    
+    self.loginButton.frame = CGRectMake(xPoint, self.passwordField.frame.origin.y + 40.0f, self.usernameField.frame.size.width, self.usernameField.frame.size.height);
 }
 
 @end
