155aa4c75ae612b4af218752bfa6e9345ffc3d24
Kevin Hwang <k3vinhwang@gmail.com>
2014-03-28 22:57:15 -0700

    Populate game view cell with titles of games

---
+++ Cantaloupe/KHGameViewCell.h
@@ -0,0 +1,13 @@
+//
+//  KHGameViewCell.h
+//  Cantaloupe
+//
+//  Created by Kevin Hwang on 3/28/14.
+//  Copyright (c) 2014 Kevin Hwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KHGameViewCell : UITableViewCell
+
+@end
--- Cantaloupe/KHGamesViewController.m
+++ Cantaloupe/KHGamesViewController.m
@@ -8,13 +8,16 @@
 
 #import "KHGamesViewController.h"
 #import "AFNetworking.h"
+#import "KHGameViewCell.h"
 
 @interface KHGamesViewController ()
 
 @property (nonatomic, strong) NSString *key;
+@property (nonatomic, strong) NSArray *games;
 
 @end
 
+static NSString *kGameCellIdentifier = @"gameCellIdentifier";
 @implementation KHGamesViewController
 
 - (id)initWithStyle:(UITableViewStyle)style key:(NSString *)key
@@ -22,6 +25,7 @@
     self = [super initWithStyle:style];
     if (self) {
         self.key = key;
+        [self.tableView registerClass:[KHGameViewCell class] forCellReuseIdentifier:kGameCellIdentifier];
         [self _requestGames];
     }
     return self;
@@ -33,6 +37,20 @@
     NSString *url = [NSString stringWithFormat:@"http://itch.io/api/1/%@/my-games", self.key];
     [manager POST:url parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
         NSLog(@"JSON: %@", responseObject);
+        
+        NSDictionary *responseDict = (NSDictionary *)responseObject;
+        
+        NSArray *errors = [responseDict valueForKey:@"errors"];
+        
+        if (errors) {
+            UIAlertView *alert = [[UIAlertView alloc] initWithTitle:NSLocalizedString(@"Sorry", nil) message:NSLocalizedString(@"Something went wrong.", nil) delegate:self cancelButtonTitle:NSLocalizedString(@"Ok", nil) otherButtonTitles:nil];
+            [alert show];
+            return;
+        }
+        NSArray *games = [responseDict valueForKey:@"games"];
+        self.games = games;
+        [self.tableView reloadData];
+        
     } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
         NSLog(@"Error: %@", error);
     }];
@@ -54,28 +72,24 @@
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-#warning Potentially incomplete method implementation.
-    // Return the number of sections.
-    return 0;
+    return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-#warning Incomplete method implementation.
-    // Return the number of rows in the section.
-    return 0;
+    return [self.games count];
 }
 
-/*
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:kGameCellIdentifier forIndexPath:indexPath];
     
-    // Configure the cell...
-    
+    UILabel *gameTitle = [[UILabel alloc] initWithFrame:cell.frame];
+    gameTitle.text = [[self.games objectAtIndex:[indexPath row]] valueForKey:@"title"];
+    [cell.contentView addSubview:gameTitle];
     return cell;
 }
-*/
+
 
 /*
 // Override to support conditional editing of the table view.
