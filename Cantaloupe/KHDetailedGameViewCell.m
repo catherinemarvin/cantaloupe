f586788035171330affcd23c61e2c8e523b48603
Kevin Hwang <k3vinhwang@gmail.com>
2014-03-31 20:10:01 -0700

    Fix bug where cells would get reused and then labels would get continuously overwritten

--- Cantaloupe/KHDetailedGameViewCell.m
+++ Cantaloupe/KHDetailedGameViewCell.m
@@ -8,15 +8,27 @@
 
 #import "KHDetailedGameViewCell.h"
 
+@interface KHDetailedGameViewCell()
+
+@property (nonatomic, strong) UILabel *infoView;
+
+@end
+
 @implementation KHDetailedGameViewCell
+
+-(id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
+    if (self = [super initWithStyle:style reuseIdentifier:reuseIdentifier]) {
+        self.infoView = [[UILabel alloc] initWithFrame:self.frame];
+        [self.contentView addSubview:self.infoView];
+    }
+    return self;
+}
 
 - (void)configureWithData:(NSDictionary *)data {
     NSString *title = [[data allKeys] objectAtIndex:0];
     NSString *contents = [[data allValues] objectAtIndex:0];
     
-    UILabel *info = [[UILabel alloc] initWithFrame:self.frame];
-    info.text = [NSString stringWithFormat:@"%@: %@", title, contents];
-    [self.contentView addSubview:info];
+    self.infoView.text = [NSString stringWithFormat:@"%@: %@", title, contents];
 }
 
 @end
