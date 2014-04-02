f6429bc17ba3ed5eb61d220eb0a4c9d16980ec2a
Kevin Hwang <k3vinhwang@gmail.com>
2014-04-01 22:53:53 -0700

    Upgrade my icons

--- Cantaloupe/KHTabBarController.m
+++ Cantaloupe/KHTabBarController.m
@@ -9,10 +9,12 @@
 #import "KHTabBarController.h"
 #import "KHGamesViewController.h"
 #import "KHPurchasesViewController.h"
+#import "KHSettingsViewController.h"
 
 typedef NS_ENUM(NSInteger, KHTabViewControllerTag) {
     KHGamesViewControllerTag,
-    KHPurchasesViewControllerTag
+    KHPurchasesViewControllerTag,
+    KHSettingsViewControllerTag
 };
 
 @interface KHTabBarController ()
@@ -20,6 +22,7 @@ typedef NS_ENUM(NSInteger, KHTabViewControllerTag) {
 @property (nonatomic, strong) NSString *key;
 @property (nonatomic, strong) KHGamesViewController *gamesViewController;
 @property (nonatomic, strong) KHPurchasesViewController *purchasesViewController;
+@property (nonatomic, strong) KHSettingsViewController *settingsViewController;
 
 @end
 
@@ -39,7 +42,11 @@ typedef NS_ENUM(NSInteger, KHTabViewControllerTag) {
         self.purchasesViewController.tabBarItem = [[UITabBarItem alloc] initWithTitle:@"Purchases" image:[UIImage imageNamed:@"purchasesIcon"] tag:KHPurchasesViewControllerTag];
         UINavigationController *purchasesNav = [[UINavigationController alloc] initWithRootViewController:self.purchasesViewController];
         
-        NSArray *viewControllers = @[gamesNav, purchasesNav];
+        self.settingsViewController = [[KHSettingsViewController alloc] initWithStyle:UITableViewStyleGrouped];
+        self.settingsViewController.tabBarItem = [[UITabBarItem alloc] initWithTitle:@"Settings" image:[UIImage imageNamed:@"settingsIcon"] tag:KHSettingsViewControllerTag];
+        UINavigationController *settingsNav = [[UINavigationController alloc] initWithRootViewController:self.settingsViewController];
+        
+        NSArray *viewControllers = @[gamesNav, purchasesNav, settingsNav];
         [self setViewControllers:viewControllers];
     }
     return self;
