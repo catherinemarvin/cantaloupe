c2f17504774fe9220b717e8f163e94d6991368ca
Kevin Hwang <k3vinhwang@gmail.com>
2014-04-03 20:18:27 -0700

    Remove KHPurchasesViewController

--- Cantaloupe/KHTabBarController.m
+++ Cantaloupe/KHTabBarController.m
@@ -8,12 +8,10 @@
 
 #import "KHTabBarController.h"
 #import "KHGamesViewController.h"
-#import "KHPurchasesViewController.h"
 #import "KHSettingsViewController.h"
 
 typedef NS_ENUM(NSInteger, KHTabViewControllerTag) {
     KHGamesViewControllerTag,
-    KHPurchasesViewControllerTag,
     KHSettingsViewControllerTag
 };
 
@@ -21,7 +19,6 @@ typedef NS_ENUM(NSInteger, KHTabViewControllerTag) {
 
 @property (nonatomic, strong) NSString *key;
 @property (nonatomic, strong) KHGamesViewController *gamesViewController;
-@property (nonatomic, strong) KHPurchasesViewController *purchasesViewController;
 @property (nonatomic, strong) KHSettingsViewController *settingsViewController;
 
 @end
@@ -38,15 +35,11 @@ typedef NS_ENUM(NSInteger, KHTabViewControllerTag) {
         self.gamesViewController.tabBarItem = [[UITabBarItem alloc] initWithTitle:@"Games" image:[UIImage imageNamed:@"gamesIcon"] tag:KHGamesViewControllerTag];
         UINavigationController *gamesNav = [[UINavigationController alloc] initWithRootViewController:self.gamesViewController];
         
-        self.purchasesViewController = [[KHPurchasesViewController alloc] init];
-        self.purchasesViewController.tabBarItem = [[UITabBarItem alloc] initWithTitle:@"Purchases" image:[UIImage imageNamed:@"purchasesIcon"] tag:KHPurchasesViewControllerTag];
-        UINavigationController *purchasesNav = [[UINavigationController alloc] initWithRootViewController:self.purchasesViewController];
-        
         self.settingsViewController = [[KHSettingsViewController alloc] initWithStyle:UITableViewStyleGrouped];
         self.settingsViewController.tabBarItem = [[UITabBarItem alloc] initWithTitle:@"Settings" image:[UIImage imageNamed:@"settingsIcon"] tag:KHSettingsViewControllerTag];
         UINavigationController *settingsNav = [[UINavigationController alloc] initWithRootViewController:self.settingsViewController];
         
-        NSArray *viewControllers = @[gamesNav, purchasesNav, settingsNav];
+        NSArray *viewControllers = @[gamesNav, settingsNav];
         [self setViewControllers:viewControllers];
     }
     return self;
