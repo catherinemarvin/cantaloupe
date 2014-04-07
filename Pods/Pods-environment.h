964843ea81644ea2f9339ba3be6735cea50d5e9f
Kevin Hwang <k3vinhwang@gmail.com>
2014-04-06 17:42:53 -0700

    Add SSPullToRefresh

--- Pods/Pods-environment.h
+++ Pods/Pods-environment.h
@@ -66,3 +66,9 @@
 #define COCOAPODS_VERSION_MINOR_SSKeychain 2
 #define COCOAPODS_VERSION_PATCH_SSKeychain 1
 
+// SSPullToRefresh
+#define COCOAPODS_POD_AVAILABLE_SSPullToRefresh
+#define COCOAPODS_VERSION_MAJOR_SSPullToRefresh 1
+#define COCOAPODS_VERSION_MINOR_SSPullToRefresh 2
+#define COCOAPODS_VERSION_PATCH_SSPullToRefresh 0
+
