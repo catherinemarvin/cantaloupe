ceba689397928cc27cd0b02785981e1a83124072
Kevin Hwang <k3vinhwang@gmail.com>
2014-04-17 21:24:05 -0700

    Install Reachability

--- Pods/Pods-environment.h
+++ Pods/Pods-environment.h
@@ -60,6 +60,12 @@
 #define COCOAPODS_VERSION_MINOR_MagicalRecord_Core 2
 #define COCOAPODS_VERSION_PATCH_MagicalRecord_Core 0
 
+// Reachability
+#define COCOAPODS_POD_AVAILABLE_Reachability
+#define COCOAPODS_VERSION_MAJOR_Reachability 3
+#define COCOAPODS_VERSION_MINOR_Reachability 1
+#define COCOAPODS_VERSION_PATCH_Reachability 1
+
 // SSKeychain
 #define COCOAPODS_POD_AVAILABLE_SSKeychain
 #define COCOAPODS_VERSION_MAJOR_SSKeychain 1
