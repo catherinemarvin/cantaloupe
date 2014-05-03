2a91603def605f693d03999f063593a6c8cc92c6
Kevin Hwang <k3vinhwang@gmail.com>
2014-05-03 11:36:58 -0700

    Remove unnecessary init code

--- Cantaloupe/KHGraphFooterView.m
+++ Cantaloupe/KHGraphFooterView.m
@@ -8,24 +8,24 @@
 
 #import "KHGraphFooterView.h"
 
+@interface KHGraphFooterView()
+
+@property (nonatomic, strong) UIView *topSeparatorView;
+
+@end
+
+
 @implementation KHGraphFooterView
 
 - (id)initWithFrame:(CGRect)frame
 {
     self = [super initWithFrame:frame];
     if (self) {
-        // Initialization code
+        self.backgroundColor = [UIColor clearColor];
+        self.footerSeparatorColor = nil;
+        
     }
     return self;
 }
-
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect
-{
-    // Drawing code
-}
-*/
 
 @end
