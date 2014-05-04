dc364a0534459c68c1f8bd23135dca467ba3cc6a
Kevin Hwang <k3vinhwang@gmail.com>
2014-05-04 16:09:09 -0700

    Add left, right labels

--- Cantaloupe/KHGraphFooterView.m
+++ Cantaloupe/KHGraphFooterView.m
@@ -24,6 +24,25 @@
         self.backgroundColor = [UIColor clearColor];
         self.footerSeparatorColor = nil;
         
+        self.topSeparatorView = [[UIView alloc] init];
+        self.topSeparatorView.backgroundColor = [UIColor grayColor];
+        [self addSubview:self.topSeparatorView];
+        
+        self.leftLabel = [[UILabel alloc] init];
+        self.leftLabel.adjustsFontSizeToFitWidth = YES;
+        self.leftLabel.font = [UIFont fontWithName:@"Lato-Regular" size:10.0f];
+        self.leftLabel.textAlignment = NSTextAlignmentLeft;
+        self.leftLabel.textColor = [UIColor whiteColor];
+        self.leftLabel.backgroundColor = [UIColor clearColor];
+        [self addSubview:self.leftLabel];
+        
+        self.rightLabel = [[UILabel alloc] init];
+        self.rightLabel.adjustsFontSizeToFitWidth = YES;
+        self.rightLabel.font = [UIFont fontWithName:@"Lato-Regular" size:10.0f];
+        self.rightLabel.textAlignment = NSTextAlignmentRight;
+        self.rightLabel.textColor = [UIColor whiteColor];
+        self.rightLabel.backgroundColor = [UIColor clearColor];
+        [self addSubview:self.rightLabel];
     }
     return self;
 }
