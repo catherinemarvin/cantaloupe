21e16cb7fa77eaa16495d38dc909a18654decaea
Kevin Hwang <k3vinhwang@gmail.com>
2014-05-13 22:52:19 -0700

    Remove unnecessary green background and prettify title label a little

--- Cantaloupe/KHGameViewCell.m
+++ Cantaloupe/KHGameViewCell.m
@@ -19,15 +19,17 @@
 
 - (id)initWithFrame:(CGRect)frame {
     if (self = [super initWithFrame:frame]) {
-        self.backgroundColor = [UIColor greenColor];
-        
         self.imageView = [[UIImageView alloc] initWithFrame:self.bounds];
         self.imageView.contentMode = UIViewContentModeScaleAspectFill;
+        [self.imageView.layer setBackgroundColor:[UIColor blackColor].CGColor];
+        [self.imageView.layer setOpacity:0.9];
         [self.imageView setClipsToBounds:YES];
         
         [self.contentView addSubview:self.imageView];
         
         self.titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, self.bounds.origin.y / 2, self.bounds.size.width, 20)];
+        self.titleLabel.font = [UIFont fontWithName:@"Lato-Regular" size:16.0f];
+        self.titleLabel.textColor = [UIColor whiteColor];
         [self.contentView addSubview:self.titleLabel];
     }
     return self;
