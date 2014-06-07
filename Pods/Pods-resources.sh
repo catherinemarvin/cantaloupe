ac8024421943eda99414581e80d595f5aa211105
Kevin Hwang <k3vinhwang@gmail.com>
2014-06-06 20:10:49 -0700

    Add Tumblr pod to grab news posts

--- Pods/Pods-resources.sh
+++ Pods/Pods-resources.sh
@@ -41,6 +41,11 @@ install_resource()
       ;;
   esac
 }
+install_resource "TMTumblrSDK/TMTumblrSDK/Activity/UIActivityTumblr.png"
+install_resource "TMTumblrSDK/TMTumblrSDK/Activity/UIActivityTumblr@2x.png"
+install_resource "TMTumblrSDK/TMTumblrSDK/Activity/UIActivityTumblr@2x~ipad.png"
+install_resource "TMTumblrSDK/TMTumblrSDK/Activity/UIActivityTumblr~ipad.png"
+install_resource "TMTumblrSDK/TMTumblrSDK/Activity/UIActivityTumblr~ipad@2x.png"
 
 rsync -avr --copy-links --no-relative --exclude '*/.svn/*' --files-from="$RESOURCES_TO_COPY" / "${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
 if [[ "${ACTION}" == "install" ]]; then
