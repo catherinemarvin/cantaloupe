4acc0b229e31d4c20e925523b68ae64f912068b4
Kevin Hwang <k3vinhwang@gmail.com>
2014-06-07 17:18:11 -0700

    Add request news option

--- Cantaloupe/KHNewsViewController.m
+++ Cantaloupe/KHNewsViewController.m
@@ -14,11 +14,10 @@
 
 @implementation KHNewsViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+- (id)initWithCollectionViewLayout:(UICollectionViewLayout *)layout {
+    self = [super initWithCollectionViewLayout:layout];
     if (self) {
-        // Custom initialization
+        [self _requestNews];
     }
     return self;
 }
@@ -29,21 +28,8 @@
     // Do any additional setup after loading the view.
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)_requestNews {
+    
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
