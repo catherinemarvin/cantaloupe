5322bac4caed49010c1651bac490ccb49a79a22f
Kevin Hwang <k3vinhwang@gmail.com>
2014-06-14 21:44:27 -0700

    Remove some unused generated code

--- Cantaloupe/KHDetailedNewsViewController.m
+++ Cantaloupe/KHDetailedNewsViewController.m
@@ -31,30 +31,12 @@
     KHNewsView *newsView = [[KHNewsView alloc] initWithFrame:[UIScreen mainScreen].bounds news:self.news];
     self.view = newsView;
     self.newsView = newsView;
-    
-    self.navigationItem.title = [self.news objectForKey:@"title"];
 }
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    self.navigationItem.title = [self.news objectForKey:@"title"];
 }
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
