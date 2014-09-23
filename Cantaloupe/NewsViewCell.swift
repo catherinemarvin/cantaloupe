be7f8743526718836780dd8d3ce201994a28e13e
Kevin Hwang <k3vinhwang@gmail.com>
2014-09-22 22:23:34 -0700

    Use Swift to display the news view cell

--- Cantaloupe/NewsViewCell.swift
+++ Cantaloupe/NewsViewCell.swift
@@ -10,18 +10,42 @@ import UIKit
 
 class NewsViewCell: UICollectionViewCell {
     
-    let titleLabel = UIButton()
+    var titleLabel:UILabel
+    
+    var news:Dictionary<NSString, AnyObject>?
     
     override init(frame: CGRect) {
+        
+        self.titleLabel = UILabel()
+        self.titleLabel.font = UIFont(name: "Lato-Regular", size: 16.0)
+        self.titleLabel.textColor = UIColor.blackColor()
         super.init(frame: frame)
+        
+        self.contentView.addSubview(self.titleLabel)
+        self.backgroundColor = UIColor.whiteColor()
+    }
+
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
     
-    required init(coder aDecoder:NSCoder) {
-        super.init(coder: aDecoder)
+    func configureWithNews(news: Dictionary<NSString, AnyObject>) -> Void {
+        self.news = news
+        
+        if let title = self.news!["title"] as? NSString {
+            self.titleLabel.text = title
+        }
+        self.setNeedsLayout()
     }
     
-//    configureWithNews(news: Dictionary) -> void {
-//    
-//    }
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        self.titleLabel.sizeToFit()
+        var titleFrame = self.titleLabel.frame
+        titleFrame.origin.x = 0
+        titleFrame.origin.y = 0
+        self.titleLabel.frame = titleFrame
+    }
     
 }
