327bf69d61acfc7b904e4e02ec5682253bd92ea7
Kevin Hwang <k3vinhwang@gmail.com>
2014-09-25 22:28:34 -0700

    KHGameViewCell -> GameViewCell

---
+++ Cantaloupe/GameViewCell.swift
@@ -0,0 +1,57 @@
+//
+//  GameViewCell.swift
+//  Cantaloupe
+//
+//  Created by Kevin Hwang on 9/25/14.
+//  Copyright (c) 2014 Kevin Hwang. All rights reserved.
+//
+
+import UIKit
+
+class GameViewCell: UICollectionViewCell {
+    
+    let imageView:UIImageView
+    let titleLabel:UILabel
+    
+    override init(frame: CGRect) {
+        self.imageView = UIImageView()
+        self.imageView.contentMode = UIViewContentMode.ScaleAspectFill
+        self.imageView.clipsToBounds = true
+        
+        let shadow = UIView(frame: self.imageView.bounds)
+        shadow.backgroundColor = UIColor.blackColor()
+        shadow.layer.opacity = 0.5
+        self.imageView.addSubview(shadow)
+        
+        self.titleLabel = UILabel()
+        self.titleLabel.font = UIFont(name: "Lato-Regular", size: 16)
+        self.titleLabel.textColor = UIColor.whiteColor()
+        
+        super.init(frame: frame)
+        
+        self.contentView.addSubview(self.imageView)
+        self.contentView.addSubview(self.titleLabel)
+        self.imageView.frame = self.bounds
+        self.titleLabel.frame = CGRectMake(10, floor(CGRectGetMinY(self.bounds) / 2), CGRectGetWidth(self.bounds) - 10, 20)
+    }
+
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setGameImage(url: String?) {
+        var image:UIImage
+        
+        if ((url) != nil) {
+            image = UIImage(data: NSData(contentsOfURL: NSURL(string: url!)))
+        } else {
+            image = UIImage(named: "placeholder")
+        }
+        self.imageView.image = image
+    }
+    
+    func setTitle(title: String) {
+        self.titleLabel.text = title
+    }
+    
+}
