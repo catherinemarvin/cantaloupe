62e75c223cee497c7b3f4478ccdc6ff915dcf314
Kevin Hwang <k3vinhwang@gmail.com>
2015-01-12 18:14:50 -0800

    Revert "Remove news view"
    This reverts commit 693f7d36674fa68f08f32bf32dd3e8b12f1422a6.

---
+++ Cantaloupe/KHNewsView.m
@@ -0,0 +1,55 @@
+//
+//  KHNewsView.m
+//  Cantaloupe
+//
+//  Created by Kevin Hwang on 6/12/14.
+//  Copyright (c) 2014 Kevin Hwang. All rights reserved.
+//
+
+#import "KHNewsView.h"
+
+@interface KHNewsView()
+
+@property (nonatomic, strong) NSDictionary *news;
+
+@property (nonatomic, strong) UILabel *title;
+@property (nonatomic, strong) UILabel *text;
+
+@end
+
+@implementation KHNewsView
+
+- (id)initWithFrame:(CGRect)frame news:(NSDictionary *)news {
+    if (self = [super initWithFrame:frame]) {
+        self.backgroundColor = [UIColor whiteColor];
+        self.news = news;
+        
+        self.title = [[UILabel alloc] init];
+        self.title.text = [self.news objectForKey:@"title"];
+        [self addSubview:self.title];
+        
+        self.text = [[UILabel alloc] init];
+        self.text.text = [self.news objectForKey:@"body"];
+        self.text.numberOfLines = 0;
+        [self addSubview:self.text];
+    }
+    return self;
+}
+
+- (void)layoutSubviews {
+    [super layoutSubviews];
+    [self.title sizeToFit];
+    
+    CGRect titleFrame = self.title.frame;
+    titleFrame.origin.x = 0;
+    titleFrame.origin.y = 0;
+    self.title.frame = titleFrame;
+    
+    CGRect textFrame = self.text.frame;
+    textFrame.origin.x = 0;
+    textFrame.origin.y = CGRectGetHeight(titleFrame) + 10.0f;
+    textFrame.size.width = CGRectGetWidth(self.frame);
+    self.text.frame = textFrame;
+    [self.text sizeToFit];
+}
+@end
--- Cantaloupe/NewsView.swift
+++ Cantaloupe/NewsView.swift
@@ -27,7 +27,7 @@ class NewsView: UIView {
         }
         
         self.text = UILabel()
-        if let body = self.news!["body"] as? NSString {
+        if let body = self.news!["title"] as? NSString {
             self.text.text = body
         }
         self.text.numberOfLines = 0
