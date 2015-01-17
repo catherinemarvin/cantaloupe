9239447f3daf06e8a25f3bf9b9f4fb6a220bf31d
Kevin Hwang <k3vinhwang@gmail.com>
2015-01-16 21:21:04 -0800

    Add some text to display if the user does not have graphs

--- Cantaloupe/KHDetailedGraphViewController.m
+++ Cantaloupe/KHDetailedGraphViewController.m
@@ -41,26 +41,47 @@
 #pragma mark - UIViewController
 
 - (void)loadView {
-    [super loadView];
-    
     UIView *view = [[UIView alloc] initWithFrame:[UIScreen mainScreen].bounds];
     view.backgroundColor = [UIColor whiteColor];
     
+    self.view = view;
+    
+    if ([self.graphData count] > 0) {
+        [self _setupGraphView];
+    } else {
+        [self _setupEmptyView];
+    }
+}
+
+- (void)_setupGraphView {
     self.graphView = [[BEMSimpleLineGraphView alloc] init];
     self.graphView.labelFont = [UIFont fontWithName:@"Lato-Regular" size:14.0f];
     self.graphView.enableYAxisLabel = YES;
     self.graphView.enablePopUpReport = YES;
     self.graphView.delegate = self;
     self.graphView.dataSource = self;
-    [view addSubview:self.graphView];
+    [self.view addSubview:self.graphView];
     
     CGFloat padding = 20.0f;
     UIEdgeInsets insets = UIEdgeInsetsMake(padding, padding, padding, padding);
     [self.graphView mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.edges.equalTo(view).with.insets(insets);
+        make.edges.equalTo(self.view).with.insets(insets);
+    }];
+}
+
+- (void)_setupEmptyView {
+    UILabel *title = [[UILabel alloc] init];
+    title.font = [UIFont fontWithName:@"Lato-Regular" size:20.0f];
+    title.text = NSLocalizedString(@"Sorry, you don't have any graph data.", nil);
+    title.numberOfLines = 0;
+    title.textAlignment = NSTextAlignmentCenter;
+    [self.view addSubview:title];
+    
+    [title mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.center.equalTo(self.view);
+        make.width.equalTo(self.view);
     }];
     
-    self.view = view;
 }
 
 
