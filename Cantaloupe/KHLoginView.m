be4f506fbd09105f02f883a28023efc6dae42344
Kevin Hwang <k3vinhwang@gmail.com>
2015-02-02 22:45:34 -0800

    Style the login form a little better

--- Cantaloupe/KHLoginView.m
+++ Cantaloupe/KHLoginView.m
@@ -11,7 +11,6 @@
 @interface KHLoginView()
 
 @property (nonatomic, strong) UIView *formContainer;
-@property (nonatomic, strong) UIView *divider;
 
 @end
 @implementation KHLoginView
@@ -27,12 +26,8 @@
         _usernameField.placeholder = NSLocalizedString(@"Username", nil);
         _usernameField.autocapitalizationType = UITextAutocapitalizationTypeNone;
         _usernameField.returnKeyType = UIReturnKeyNext;
+        _usernameField.backgroundColor = [UIColor colorFromHexString:@"F2F2F2"];
         [_formContainer addSubview:_usernameField];
-        
-        _divider = [[UIView alloc] init];
-        _divider.backgroundColor = [UIColor whiteColor];
-        [self addSubview:_divider];
-        
         
         _passwordField = [[UITextField alloc] init];
         _passwordField.font = [UIFont fontWithName:@"Lato-Regular" size:16.0f];
@@ -40,11 +35,12 @@
         _passwordField.secureTextEntry = YES;
         _passwordField.autocapitalizationType = UITextAutocapitalizationTypeNone;
         _passwordField.returnKeyType = UIReturnKeyGo;
+        _passwordField.backgroundColor = [UIColor colorFromHexString:@"F2F2F2"];
         [_formContainer addSubview:_passwordField];
         
         _loginButton = [[UIButton alloc] init];
         [_loginButton setTitle:NSLocalizedString(@"Sign in", nil) forState:UIControlStateNormal];
-        _loginButton.titleLabel.font = [UIFont fontWithName:@"Lato-Regular" size:28.0f];
+        _loginButton.titleLabel.font = [UIFont fontWithName:@"Lato-Regular" size:20.0f];
         [_loginButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
         [_loginButton setBackgroundColor:[UIColor linkColor]];
         [_formContainer addSubview:_loginButton];
@@ -56,35 +52,32 @@
 }
 
 - (void)updateConstraints {
-    CGFloat margin = 20.f;
+    CGFloat margin = 10.f;
+    CGFloat verticalHeight = 44.0f;
     [self.formContainer mas_updateConstraints:^(MASConstraintMaker *make) {
         make.left.equalTo(self).with.offset(margin);
         make.right.equalTo(self).with.offset(-margin);
         make.centerY.equalTo(self);
-        make.height.equalTo(@100);
     }];
     
     [self.usernameField mas_updateConstraints:^(MASConstraintMaker *make) {
         make.left.equalTo(self.formContainer).with.offset(margin);
         make.right.equalTo(self.formContainer).with.offset(-margin);
-        make.top.equalTo(self.formContainer);
-    }];
-    
-    [self.divider mas_updateConstraints:^(MASConstraintMaker *make) {
-        make.left.and.right.equalTo(self.formContainer);
-        make.top.equalTo(self.usernameField.mas_bottom);
-        make.height.equalTo(@1);
+        make.top.equalTo(self.formContainer).with.offset(margin);
+        make.height.equalTo(@(verticalHeight));
     }];
     
     [self.passwordField mas_updateConstraints:^(MASConstraintMaker *make) {
         make.left.and.right.equalTo(self.usernameField);
-        make.top.equalTo(self.divider.mas_bottom);
+        make.top.equalTo(self.usernameField.mas_bottom).with.offset(margin);
+        make.height.equalTo(@(verticalHeight));
     }];
     
     [self.loginButton mas_updateConstraints:^(MASConstraintMaker *make) {
         make.left.and.right.equalTo(self.usernameField);
-        make.top.equalTo(self.passwordField.mas_bottom);
-        make.bottom.equalTo(self.formContainer);
+        make.top.equalTo(self.passwordField.mas_bottom).with.offset(margin);
+        make.bottom.equalTo(self.formContainer).with.offset(-margin);
+        make.height.equalTo(@(verticalHeight));
     }];
     [super updateConstraints];
 }
