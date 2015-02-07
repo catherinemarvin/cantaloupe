46676ceec022fa26a1fcbb33e0876ab547ae0896
Kevin Hwang <k3vinhwang@gmail.com>
2015-02-06 21:12:50 -0800

    Add some delays

--- fastlane/snapshot.js
+++ fastlane/snapshot.js
@@ -12,12 +12,16 @@ target.frontMostApp().keyboard().typeString("khwang");
 target.frontMostApp().mainWindow().scrollViews()[0].secureTextFields()[0].secureTextFields()[0].tap();
 target.frontMostApp().keyboard().typeString("asdfasdf");
 target.frontMostApp().mainWindow().scrollViews()[0].buttons()["Sign in"].tap();
+target.delay(5);
 captureLocalizedScreenshot('0-games')
 target.tap({x:155.00, y:277.50});
+target.delay(5);
 captureLocalizedScreenshot('1-detailedgame')
 target.frontMostApp().tabBar().buttons()["Graphs"].tap();
 target.frontMostApp().mainWindow().tableViews()[0].tapWithOptions({tapOffset:{x:0.23, y:0.26}});
+target.delay(5);
 captureLocalizedScreenshot('2-graph')
 target.frontMostApp().tabBar().buttons()["News"].tap();
+target.delay(5);
 captureLocalizedScreenshot('3-news')
 
