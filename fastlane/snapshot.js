c3ef6f8e491c714ae46fe8da1990c0441a2aaabc
Kevin Hwang <k3vinhwang@gmail.com>
2015-02-06 21:01:27 -0800

    Add screenshot suite

--- fastlane/snapshot.js
+++ fastlane/snapshot.js
@@ -5,9 +5,19 @@ var app = target.frontMostApp();
 var window = app.mainWindow();
 
 
-target.delay(3)
-captureLocalizedScreenshot("0-LandingScreen")
+var target = UIATarget.localTarget();
 
-target.frontMostApp().tabBar().buttons()[1].tap();
-target.delay(1)
-captureLocalizedScreenshot("1-SecondScreen")
+target.frontMostApp().mainWindow().scrollViews()[0].textFields()[0].textFields()[0].tap();
+target.frontMostApp().keyboard().typeString("khwang");
+target.frontMostApp().mainWindow().scrollViews()[0].secureTextFields()[0].secureTextFields()[0].tap();
+target.frontMostApp().keyboard().typeString("asdfasdf");
+target.frontMostApp().mainWindow().scrollViews()[0].buttons()["Sign in"].tap();
+captureLocalizedScreenshot('0-games')
+target.tap({x:155.00, y:277.50});
+captureLocalizedScreenshot('1-detailedgame')
+target.frontMostApp().tabBar().buttons()["Graphs"].tap();
+target.frontMostApp().mainWindow().tableViews()[0].tapWithOptions({tapOffset:{x:0.23, y:0.26}});
+captureLocalizedScreenshot('2-graph')
+target.frontMostApp().tabBar().buttons()["News"].tap();
+captureLocalizedScreenshot('3-news')
+
