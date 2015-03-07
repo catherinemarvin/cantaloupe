5a0a3db98514925442d0a1f8781053900770ce52
Kevin Hwang <k3vinhwang@gmail.com>
2015-03-06 19:42:37 -0800

    Begin organizing code in modules and begin work on the new games view

---
+++ Cantaloupe/KHMyGamesViewController.h
@@ -0,0 +1,13 @@
+//
+//  KHMyGamesViewController.h
+//  Cantaloupe
+//
+//  Created by Kevin Hwang on 3/6/15.
+//  Copyright (c) 2015 Kevin Hwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KHMyGamesViewController : UIViewController
+
+@end
---
+++ Cantaloupe/KHMyGamesViewController.m
@@ -0,0 +1,37 @@
+//
+//  KHMyGamesViewController.m
+//  Cantaloupe
+//
+//  Created by Kevin Hwang on 3/6/15.
+//  Copyright (c) 2015 Kevin Hwang. All rights reserved.
+//
+
+#import "KHMyGamesViewController.h"
+
+@interface KHMyGamesViewController ()
+
+@end
+
+@implementation KHMyGamesViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
