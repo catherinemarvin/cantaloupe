83aa20a846777d0194413e3302db0743640a7cb7
Kevin Hwang <k3vinhwang@gmail.com>
2015-03-08 15:42:57 -0700

    Start hooking up the games VC to the data source

--- Cantaloupe/KHMyGamesViewController.m
+++ Cantaloupe/KHMyGamesViewController.m
@@ -6,12 +6,18 @@
 //  Copyright (c) 2015 Kevin Hwang. All rights reserved.
 //
 
+// Views
 #import "KHMyGamesViewController.h"
 #import "KHGameViewCell.h"
 
-@interface KHMyGamesViewController ()
+// Data Source
+#import "KHMyGamesDataSource.h"
 
+@interface KHMyGamesViewController ()<UICollectionViewDataSource, UICollectionViewDelegate, KHMyGamesDataSourceDelegate>
+
+@property (nonatomic, strong) UICollectionView *collectionView;
 @property (nonatomic, strong) UIRefreshControl *refreshControl;
+@property (nonatomic, strong) KHMyGamesDataSource *dataSource;
 
 @end
 
@@ -21,10 +27,69 @@ static NSString *KHkGameCellIdentifier = @"gameCellIdentifier";
 
 - (instancetype)init {
     if (self = [super init]) {
-        _refreshControl = [[UIRefreshControl alloc] init];
-        [_refreshControl addTarget:self action:@selector(_refresh) forControlEvents:UIControlEventValueChanged];
+        _dataSource = [[KHMyGamesDataSource alloc] initWithDelegate:self];
     }
     return self;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self _setupCollectionView];
+    [self _setupRefreshControl];
+    [self _setupNavigationBar];
+    [self.dataSource requestGames];
+}
+
+- (void)_setupCollectionView {
+    self.collectionView = [[UICollectionView alloc] initWithFrame:[UIScreen mainScreen].bounds collectionViewLayout:[[UICollectionViewFlowLayout alloc] init]];
+    [self.collectionView registerClass:[KHGameViewCell class] forCellWithReuseIdentifier:KHkGameCellIdentifier];
+    self.collectionView.delegate = self;
+    self.collectionView.dataSource = self;
+    [self.view addSubview:self.collectionView];
+}
+
+- (void)_setupRefreshControl {
+    self.refreshControl = [[UIRefreshControl alloc] init];
+    [self.refreshControl addTarget:self action:@selector(_refresh) forControlEvents:UIControlEventValueChanged];
+    [self.collectionView addSubview:self.refreshControl];
+}
+
+- (void)_setupNavigationBar {
+    self.navigationItem.title = NSLocalizedString(@"My Games", nil);
+}
+
+#pragma mark - UICollectionViewDataSource
+
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
+    return 1;
+}
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return [self.dataSource count];
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:KHkGameCellIdentifier forIndexPath:indexPath];
+    if ([cell isKindOfClass:[KHGameViewCell class]]) {
+        KHGameViewCell *gameCell = (KHGameViewCell *)cell;
+#warning TODO: Configure cell with KHGameInfo from data source
+        
+    }
+    return cell;
+}
+
+#pragma mark - UICollectionViewDelegate
+
+#pragma mark - UIRefreshControl
+
+- (void)_refresh {
+    [self.dataSource requestGames];
+}
+
+#pragma mark - KHMyGamesDataSourceDelegate
+
+- (void)gamesFetched {
+    [self.collectionView reloadData];
 }
 
 @end
