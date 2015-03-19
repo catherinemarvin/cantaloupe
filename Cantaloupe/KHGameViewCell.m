8b4573f67e1c6a7984b86f81cb09fdef6d714e9b
Kevin Hwang <k3vinhwang@gmail.com>
2015-03-18 22:02:05 -0700

    Add price label, views, and downloads label

--- Cantaloupe/KHGameViewCell.m
+++ Cantaloupe/KHGameViewCell.m
@@ -24,6 +24,10 @@
 @property (nonatomic, strong) UILabel *titleLabel;
 @property (nonatomic, strong) UILabel *descriptionLabel;
 
+@property (nonatomic, strong) UILabel *priceLabel;
+@property (nonatomic, strong) UILabel *viewsLabel;
+@property (nonatomic, strong) UILabel *downloadsLabel;
+
 @end
 
 @implementation KHGameViewCell
@@ -46,6 +50,15 @@
         _descriptionLabel = [[UILabel alloc] init];
         _descriptionLabel.font = [UIFont regularWithSize:12.0f];
         [_footer addSubview:_descriptionLabel];
+        
+        _priceLabel = [[UILabel alloc] init];
+        [_footer addSubview:_priceLabel];
+        
+        _viewsLabel = [[UILabel alloc] init];
+        [_footer addSubview:_viewsLabel];
+        
+        _downloadsLabel = [[UILabel alloc] init];
+        [_footer addSubview:_downloadsLabel];
         
         [self _initializeAutolayout];
     }
@@ -73,12 +86,30 @@
         make.top.equalTo(self.titleLabel.mas_bottom);
         make.left.and.right.equalTo(self.titleLabel);
     }];
+    
+    [self.priceLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.descriptionLabel.mas_bottom);
+        make.left.equalTo(self.titleLabel);
+    }];
+    
+    [self.viewsLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.priceLabel);
+        make.left.equalTo(self.priceLabel.mas_right);
+    }];
+    
+    [self.downloadsLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.priceLabel);
+        make.left.equalTo(self.viewsLabel.mas_right);
+    }];
 }
 
 - (void)configureWithGameInfo:(KHGameInfo *)gameInfo {
     self.imageView.image = gameInfo.coverImage;
     self.titleLabel.text = gameInfo.titleString;
     self.descriptionLabel.text = gameInfo.shortDescription;
+    self.priceLabel.text = gameInfo.priceString;
+    self.viewsLabel.text = [NSString stringWithFormat:@"%ld views", gameInfo.views];
+    self.downloadsLabel.text = [NSString stringWithFormat:@"%ld downloads", gameInfo.downloads];
 }
 
 @end
