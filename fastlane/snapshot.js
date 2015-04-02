72ad0c813e7f16fa7578a4a4c52c3e08ab627a00
Kevin Hwang <k3vinhwang@gmail.com>
2015-04-01 21:28:43 -0700

    Fix screenshot capture code

--- fastlane/snapshot.js
+++ fastlane/snapshot.js
@@ -7,21 +7,23 @@ var window = app.mainWindow();
 
 var target = UIATarget.localTarget();
 
-target.frontMostApp().mainWindow().scrollViews()[0].textFields()[0].textFields()[0].tap();
+target.frontMostApp().mainWindow().scrollViews()[0].images()["login_background.jpg"].textFields()[0].textFields()[0].tap();
 target.frontMostApp().keyboard().typeString("khwang");
-target.frontMostApp().mainWindow().scrollViews()[0].secureTextFields()[0].secureTextFields()[0].tap();
+target.frontMostApp().mainWindow().scrollViews()[0].images()["login_background.jpg"].secureTextFields()[0].secureTextFields()[0].tap();
 target.frontMostApp().keyboard().typeString("asdfasdf");
-target.frontMostApp().mainWindow().scrollViews()[0].buttons()["Sign in"].tap();
+target.frontMostApp().mainWindow().scrollViews()[0].images()["login_background.jpg"].buttons()["Sign in"].tap();
 target.delay(5);
-captureLocalizedScreenshot('0-games')
-target.tap({x:155.00, y:277.50});
+captureLocalizedScreenshot('0-games');
+target.tap({x:148.50, y:213.50});
 target.delay(5);
-captureLocalizedScreenshot('1-detailedgame')
+captureLocalizedScreenshot('1-detailedgame');
 target.frontMostApp().tabBar().buttons()["Graphs"].tap();
-target.frontMostApp().mainWindow().tableViews()[0].tapWithOptions({tapOffset:{x:0.23, y:0.26}});
 target.delay(5);
-captureLocalizedScreenshot('2-graph')
+captureLocalizedScreenshot('2-graph');
 target.frontMostApp().tabBar().buttons()["News"].tap();
 target.delay(5);
-captureLocalizedScreenshot('3-news')
+captureLocalizedScreenshot('3-news');
+target.frontMostApp().tabBar().buttons()["Settings"].tap();
+target.delay(5);
+captureLocalizedScreenshot('4-settings');
 
